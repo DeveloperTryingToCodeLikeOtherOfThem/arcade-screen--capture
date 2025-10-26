33930d9ba0dcf0cdd2cfbc2ffa24d7b92159ae31
a012 <Aeagan0507@gmail.com>
2025-10-26 18:41:15 -0400

    Update main.ts, screenCapture.ts

--- screenCapture.ts
+++ screenCapture.ts
@@ -19,7 +19,7 @@ namespace screenCapture {
     }
 
    //% weight=99
-    //% blockId=screencapturimagedatainscreen160x120 block="capture image data in screen 160 x 120" 
+    //% blockId=screenCaptureCreateCaptureScreenImageSprite block="capture image data in screen 160 x 120" 
     //% blockAliasFor=screenCapture.captureImageDataInScreen160x120
     export function __captureImageDataInScreen160x120(): Sprite {
         let img: Image = image.create(screen.width, screen.height) as ScreenImage
