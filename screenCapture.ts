40240c0d5c81764d3ae45b70d739383fd023d0f8
a012 <Aeagan0507@gmail.com>
2025-10-26 18:29:30 -0400

    Fixed weight issues

--- screenCapture.ts
+++ screenCapture.ts
@@ -1,6 +1,27 @@
-
+//% blockGap=8 block="screen capture"
 namespace screenCapture {
-    export function captureImageDataInScreen160x120(): Image {
+    //% blockId=screenCaptureCreateCaptureScreenImageSprite 
+    //% block="create capture screen image sprite"
+    //%  blockSetVariable=myScreenCaptureSprite
+    //% weight=100
+    export function createCaptureScreenImageSprite(): Sprite {
+        let img: Image = image.create(screen.width, screen.height) as ScreenImage
+
+        for (let y = 0; y < screen.height; y++)
+            for (let x = 0; x < screen.width; x++) {
+                let color = screen.getPixel(x, y)
+                img.setPixel(x, y, color) // blits the sprites first position and makes the copied one static
+                scene.centerCameraAt(x, y)
+            }
+        
+        const sprite: Sprite = new Sprite(img)
+        return sprite
+    }
+
+   //% weight=99
+    //% blockId=screencapturimagedatainscreen160x120 block="capture image data in screen 160 x 120" 
+    //% blockAliasFor=screenCapture.captureImageDataInScreen160x120
+    export function __captureImageDataInScreen160x120(): Image {
         let img: Image = image.create(screen.width, screen.height) as ScreenImage
 
         for (let y = 0; y < screen.height; y++)
