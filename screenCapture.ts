a6484ecb0288f4852a2d0304716b4e00fc71f646
a012 <Aeagan0507@gmail.com>
2025-10-26 20:50:58 -0400

    Update screenCapture.ts

--- screenCapture.ts
+++ screenCapture.ts
@@ -35,5 +35,5 @@ namespace screenCapture {
     @param ms means the number of milliseconds ordered for the screen paused for
     */
     //% weight=98
-    export function pause(ms: number = 0) { }
+    export function pause(ms: number) {}
 }
