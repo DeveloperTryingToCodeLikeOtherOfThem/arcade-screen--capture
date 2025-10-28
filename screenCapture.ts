3fb47d13751b33043ba1b351aff85f638a2782d0
a012 <Aeagan0507@gmail.com>
2025-10-27 22:14:37 -0400

    Update pxt.json, screenCapture.ts

--- screenCapture.ts
+++ screenCapture.ts
@@ -21,6 +21,13 @@ namespace screenCapture {
 
     function getImage(): Image {
         let img: Image = image.create(screen.width, screen.height) as ScreenImage
+        for (let y = 0; y < screen.height; y++)
+            for (let x = 0; x < screen.width; x++) {
+                let color = screen.getPixel(x, y)
+                game.onUpdate(() => {
+                    img.setPixel(x += screen.width, y += screen.height, color) // blits the sprites first position and makes the copied one static
+                })
+            }
         
         // updated I did this because this is a new version if sprites are out of screen then updates every frame anything inside the camera and screen configure
         game.onUpdate(() => {
